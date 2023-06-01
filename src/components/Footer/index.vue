66c65933a4174505775af2fa94936294e69d0df1
wjh <2044833571@qq.com>
2023-06-01 14:29:45 +0800

    又修改了一些信息

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -7,13 +7,9 @@
       </span>
       <!-- 以下信息请不要修改哦 -->
       <span class="hidden"
-        >&nbsp;&amp;&nbsp;Made&nbsp;by&nbsp;<a
-          :href="config.github"
-          target="_blank"
-        >
+        >&nbsp;&amp;&nbsp;Made&nbsp;by&nbsp;
           {{ config.author }}
-        </a></span
-      >
+        </span>
       <!-- &nbsp;&amp; -->
       <!-- 站点备案 -->
       <!-- <a href="https://beian.miit.gov.cn" target="_blank">豫ICP备2022018134号-1</a> -->
